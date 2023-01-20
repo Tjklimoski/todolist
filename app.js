d1965a856ab3d75f95f7436359732f9ebacb38fd
TJ Klimoski <tjklimoski@gmail.com>
2023-01-19 19:38:47 -0500

    comment day of week function

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const __dirname = dirname(__filename);
 app.get('/', (req, res) => {
   const today = new Date();
 
+  // .getDat() returns a number coresponding to the day
+  // 6 === Saturday, 0 === Sunday
   if (today.getDay() === 6 || today.getDay() === 0) {
     res.send('Yay, it\'s the weekend!');
   } else {
