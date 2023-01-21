5fa4b29ecfe63764d527c8906e359a94735bb652
TJ Klimoski <tjklimoski@gmail.com>
2023-01-21 14:44:23 -0500

    Implement get and post for /work route

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const __dirname = dirname(__filename);
 
 // set global variable
 let tasks = ['task 1', 'task 2', 'task 3'];
+let workTasks = ['work task 1', 'a really long work task 2 to see how the page handles the formatting of long piece of text lol.', 'work task 3'];
 
 // set app view engine to EJS
 app.set('view engine', 'ejs');
@@ -38,10 +39,22 @@ app.get('/', (req, res) => {
   res.render('list', {title: title, tasks: tasks});
 });
 
+app.get('/work', (req, res) => {
+  let title = 'Work List';
+
+  res.render('list', {title: title, tasks: workTasks});
+});
+
 app.post('/', (req, res) => {
   let task = req.body.task;
   tasks.push(task);
   res.redirect('/');
+});
+
+app.post('/work', (req, res) => {
+  let task = req.body.task;
+  workTasks.push(task);
+  res.redirect('/work');
 });
 
 app.listen(port, (err) => {
